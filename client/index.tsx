b615047f1c2461773b58b1782a458df5a8fdefcd
Matt Nedrich <mattnedrich@gmail.com>
2017-06-25 11:32:21 -0400

    updated dependencies, added redux saga

--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/app';
 import updateState from './reducers/index';
 
+import createSagaMiddleware from 'redux-saga'
+const sagaMiddleware = createSagaMiddleware();
+
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(updateState, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(updateState, composeEnhancers(applyMiddleware(sagaMiddleware)));
+
+import sagas from './sagas';
+
+sagaMiddleware.run(sagas);
 
 ReactDom.render(
   <Provider store={store}>
