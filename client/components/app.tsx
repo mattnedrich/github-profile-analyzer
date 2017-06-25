9dcbdd03babc216381249291b41f52d164d80ea6
Matt Nedrich <mattnedrich@gmail.com>
2017-06-25 13:09:34 -0400

    skeleton wip

--- client/components/app.tsx
+++ client/components/app.tsx
@@ -9,9 +9,19 @@ class App extends React.Component<any, any> {
   render() {
     return (
       <div>
-        <div id="profile-container">test</div>
         <div style={{textAlign: 'center'}}>
           <EnterUsername />
+        </div>
+        <div id="profile">
+          <div id="profile-photo">
+            <div id="profile-photo-image"><img src="https://avatars2.githubusercontent.com/u/4796480?v=3&s=460" /></div>
+          </div>
+          <div id="profile-details">
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+            <div>Item 4</div>
+          </div>
         </div>
       </div>
     );
