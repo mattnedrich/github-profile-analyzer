edd83b47f69699e00a2965e6bf85b9c0c963a58e
Matt Nedrich <mattnedrich@gmail.com>
2017-07-09 12:06:50 -0400

    styled profile section

--- client/components/user-profile.tsx
+++ client/components/user-profile.tsx
@@ -18,17 +18,42 @@ export default class UserProfile extends React.Component<Props, any> {
     const reposForked = this.props.reposForked || 0;
     const mostPopularLanguage = this.props.mostUsedLanguage ? this.props.mostUsedLanguage.displayName : "";
     const mostStarredRepository = this.props.mostStarredRepo ? this.props.mostStarredRepo.name : "";
+    const mostStarredRepositoryUrl = this.props.mostStarredRepo ? this.props.mostStarredRepo.url: "";
+
     const mostForkedRepository = this.props.mostForkedRepo ? this.props.mostForkedRepo.name : "";
+    const mostForkedRepositoryUrl = this.props.mostForkedRepo ? this.props.mostForkedRepo.url: "";
+
     const newestRepository = this.props.newestRepo ? this.props.newestRepo.name : "";
+    const newestRepositoryUrl = this.props.newestRepo ? this.props.newestRepo.url: "";
 
     return (
       <div id="profile-details">
-        <div>{reposAuthored} Repositories Authored</div>
-        <div>{reposForked} Repositories Forked</div>
-        <div>Most Popular Language: {mostPopularLanguage}</div>
-        <div>Most Starred Repository: {mostStarredRepository}</div>
-        <div>Most Forked Repository: {mostForkedRepository}</div>
-        <div>Most Recent Repository: {newestRepository}</div>
+        <div className="profile-detail-item-with-icon">
+          <i className="fa fa-github-square"></i>
+          <div className="profile-details-item">{reposAuthored} Repositories Authored</div>
+        </div>
+        <div className="profile-detail-item-with-icon">
+          <i className="fa fa-code-fork"></i>
+          <div className="profile-details-item">{reposForked} Repositories Forked</div>
+        </div>
+        <div className="profile-detail-item-with-icon">
+          <i className="fa fa-star"></i>
+          <div className="profile-details-item">
+            Most Starred: <a href={mostStarredRepositoryUrl}>{mostStarredRepository}</a>
+          </div>
+        </div>
+        <div className="profile-detail-item-with-icon">
+          <i className="fa fa-code-fork"></i>
+          <div className="profile-details-item">
+            Most Forked: <a href={mostForkedRepositoryUrl}>{mostForkedRepository}</a>
+          </div>
+        </div>
+        <div className="profile-detail-item-with-icon">
+          <i className="fa fa-clock-o"></i>
+          <div className="profile-details-item">
+            Most Recent: <a href={newestRepositoryUrl}>{newestRepository}</a>
+          </div>
+        </div>
       </div>
     );
   }
